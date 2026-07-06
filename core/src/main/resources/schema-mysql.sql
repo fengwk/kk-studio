ffeafeb48b8e7bae93d74197d7c99a62f3f89dae
fengwk <759543714@qq.com>
2026-07-06 21:32:42 +0800

    chore(repo): remove residual demo artifacts

--- core/src/main/resources/schema-mysql.sql
+++ core/src/main/resources/schema-mysql.sql
@@ -1,12 +1,3 @@
-create table if not exists demo (
-    id             bigint not null comment '主键',
-    name           varchar(16) not null comment '名称',
-    gmt_create     datetime(3) not null default current_timestamp(3) comment '创建时间',
-    gmt_modified   datetime(3) not null default current_timestamp(3) comment '更新时间',
-    version        bigint not null default '0' comment '数据版本号',
-    primary key (id)
-) engine=InnoDB default charset=utf8mb4 comment='demo';
-
 create table if not exists agent_provider (
     id                          bigint not null comment '主键',
     name                        varchar(64) not null comment 'provider 名称',
